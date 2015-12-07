3c7d1e3f16b91044474dfcc47c89722c374e281a
andars <afoote97@gmail.com>
2015-12-06 17:05:01 -0700

    day 2 part 2

--- day2.rb
+++ day2.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 
 total = 0
+total_ribbon = 0
 
 STDIN.each_line do |line|
   line.chomp!
   sizes = line.split('x').map(&:to_i)
   areas = sizes.zip(sizes.rotate).map { |a| a.reduce :* }
   wrapping = areas.reduce(&:+)*2 + areas.min
+  sizes = sizes.sort
+  ribbon = 2*(sizes[0] + sizes[1]) + sizes.reduce(:*)
 
   total += wrapping
+  total_ribbon += ribbon
 end
 
-puts total
+puts "Wrapping paper: #{total}"
+puts "Ribbon: #{total_ribbon}"
