985e1801c16755db69b5919f2fbc6a4089e498e0
andars <afoote97@gmail.com>
2015-12-15 00:37:33 -0700

    Use closures for day 7

--- day7.pl
+++ day7.pl
@@ -13,17 +13,17 @@ sub is_numeric {
 while(<>) {
     my @tokens = split;
     if (/AND/) {
-        $circuit{$tokens[4]} = ['AND', $tokens[0], $tokens[2]];
+        $circuit{$tokens[4]} = sub { return value($tokens[0]) & value($tokens[2])};
     } elsif (/OR/) {
-        $circuit{$tokens[4]} = ['OR', $tokens[0], $tokens[2]];
+        $circuit{$tokens[4]} = sub { return value($tokens[0]) | value($tokens[2])};
     } elsif (/NOT/) {
-        $circuit{$tokens[3]} = ['NOT', $tokens[1]];
+        $circuit{$tokens[3]} = sub { return ~value($tokens[1]) };
     } elsif (/LSHIFT/) {
-        $circuit{$tokens[4]} = ['LSHIFT', $tokens[0], $tokens[2]];
+        $circuit{$tokens[4]} = sub { return value($tokens[0]) << value($tokens[2])};
     } elsif (/RSHIFT/) {
-        $circuit{$tokens[4]} = ['RSHIFT', $tokens[0], $tokens[2]];
+        $circuit{$tokens[4]} = sub { return value($tokens[0]) >> value($tokens[2]) };
     } else {
-        $circuit{$tokens[2]} = [$tokens[0]];
+        $circuit{$tokens[2]} = sub { return value($tokens[0])};
     }
 }
 
@@ -54,23 +54,9 @@ sub value {
     if (cached($key)) {
         return cache($key);
     }
-    my @el = @{ $circuit{$key} };
 
-    for ($el[0]) {
-        if (/AND/) {
-            return cache($key, value($el[1]) & value($el[2]));
-        } elsif (/OR/) {
-            return cache($key, value($el[1]) | value($el[2]));
-        } elsif (/NOT/) {
-            return cache($key, ~value($el[1]) & 0xffff);
-        } elsif (/LSHIFT/) {
-            return cache($key, value($el[1]) << $el[2]);
-        } elsif (/RSHIFT/) {
-            return cache($key, value($el[1]) >> $el[2]);
-        } else {
-            return value($el[0]);
-        }
-    }
+    my $value = $circuit{$key}->();
+    return cache($key, $value); 
 }
 
 my $val_a = value('a');
@@ -78,6 +64,6 @@ print "part 1: value of a: " . $val_a . "\n";
 
 %cache = ();
 
-$circuit{'b'} = [($val_a)];
+$circuit{'b'} = sub { return $val_a};
 
 print "part 2: value of a: " . value('a') . "\n";
