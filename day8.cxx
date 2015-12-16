1ac88763721bff4b3e5d8177e3d2416c6224c119
andars <afoote97@gmail.com>
2015-12-16 14:19:41 -0700

    day8 part 2

--- day8.cxx
+++ day8.cxx
@@ -10,9 +10,13 @@ int main() {
     std::string line;
     int total_code = 0;
     int total_mem = 0;
+    int total_enc = 0;
+
     while (cin >> line) {
         int code = 0;
         int mem = -2;
+        int enc = 2;
+
         bool is_escaped = false;
         for (int i = 0; i<line.length(); i++) {
             code++;
@@ -28,9 +32,18 @@ int main() {
             } else {
                 mem++;
             }
+
+            if (line[i] == '\"') {
+                enc++;
+            } else if (line[i] == '\\') {
+                enc++;
+            }
         }
+
         total_code += code;
         total_mem += mem;
+        total_enc += code + enc;
     } 
     cout << (total_code - total_mem) << endl;
+    cout << (total_enc - total_code) << endl;
 }
