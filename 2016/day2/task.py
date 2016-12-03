c9a32d9ba24917adc7d2828f734c0b432c21e7da
andars <afoote97@gmail.com>
2016-12-02 21:54:33 -0700

    refactor day 2 a bit

--- 2016/day2/task.py
+++ 2016/day2/task.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-def coords_to_num(x,y):
+def coords_to_num1(x,y):
     return 3*y + x + 1
 
-center = [1,3,7,11,13]
+row_center = [1,3,7,11,13]
 
 def coords_to_num2(x,y):
-    return center[y+2] + x
+    return row_center[y+2] + x
 
 def valid(x,y):
     return 0 <= x and x <= 2 and 0 <= y and y <= 2
@@ -21,7 +21,7 @@ moves = {
     'R': [1,0],
 }
 
-def next_coords(x, y, move):
+def next_coords1(x, y, move):
     nx,ny = map(sum,zip((x,y),moves[move]))
     return [nx,ny] if valid(nx,ny) else [x,y]
 
@@ -29,34 +29,28 @@ def next_coords2(x, y, move):
     nx,ny = map(sum, zip((x,y), moves[move]))
     return [nx,ny] if valid2(nx,ny) else [x,y]
 
-def solve(line, ix, iy):
-    directives = list(line.strip())
-    x,y = ix,iy
-    for move in directives:
-        x,y = next_coords(x,y,move)
-    return [x,y]
+next_coords = [next_coords1, next_coords2]
+coords_to_num  = [coords_to_num1, coords_to_num2]
 
-def solve2(line, ix, iy):
+def solve(line, ix, iy, part):
     directives = list(line.strip())
     x,y = ix,iy
     for move in directives:
-        x,y = next_coords2(x,y,move)
-    return [x,y]
+        x,y = next_coords[part](x,y,move)
+    return x, y, coords_to_num[part](x,y)
 
 with open('input.txt') as input:
-    x = 1
-    y = 1
+    x1 = 1
+    y1 = 1
+    x2 = -2
+    y2 = 0
     part1 = []
-    for line in input:
-        x,y = solve(line,x,y)
-        part1.append(str(coords_to_num(x,y)))
-    print(''.join(part1))
-
-with open('input.txt') as input:
-    x = -2
-    y = 0
     part2 = []
     for line in input:
-        x,y = solve2(line,x,y)
-        part2.append('{:x}'.format(coords_to_num2(x,y)).upper())
+        x1,y1, digit1 = solve(line,x1,y1,0)
+        x2,y2, digit2 = solve(line,x2,y2,1)
+        part1.append(str(digit1))
+        part2.append('{:x}'.format(digit2).upper())
+    print(''.join(part1))
     print(''.join(part2))
+
