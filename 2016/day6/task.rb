c892d794ff84ae4c5687096af927025d656fde79
andars <afoote97@gmail.com>
2016-12-11 20:54:35 -0700

    remove indent on day6 solution

--- 2016/day6/task.rb
+++ 2016/day6/task.rb
@@ -1,26 +1,26 @@
-    #!/usr/bin/env ruby
-    filename = ARGV[0]
+#!/usr/bin/env ruby
+filename = ARGV[0]
 
-    positions = []
-    10.times do |i|
-      positions << Hash.new(0)
-    end
+positions = []
+10.times do |i|
+  positions << Hash.new(0)
+end
 
-    File.open(filename).each_line do |line|
-      chars = line.split('')
-      chars.each_with_index do |c, i|
-        positions[i][c] += 1
-      end
-    end
+File.open(filename).each_line do |line|
+  chars = line.split('')
+  chars.each_with_index do |c, i|
+    positions[i][c] += 1
+  end
+end
 
-    puts "Part 1:"
-    puts positions.map { |pos|
-      k,_ = pos.max_by{|k,v| v}
-      k
-    }.join
+puts "Part 1:"
+puts positions.map { |pos|
+  k,_ = pos.max_by{|k,v| v}
+  k
+}.join
 
-    puts "Part 2:"
-    puts positions.map { |pos|
-      k,_ = pos.min_by{|k,v| v}
-      k
-    }.join
+puts "Part 2:"
+puts positions.map { |pos|
+  k,_ = pos.min_by{|k,v| v}
+  k
+}.join
