015f7164f600a8f327b787cdb19eb102490f8115
andars <afoote97@gmail.com>
2016-12-11 20:52:49 -0700

    update day 10 solution

--- 2016/day10/task.rb
+++ 2016/day10/task.rb
@@ -5,19 +5,17 @@ require 'set'
 filename = ARGV[0]
 
 lines = File.readlines(filename)
-goal = [17,61].to_set
+goal = [17,61]
 
-bots = Hash.new
-instrs = Hash.new
-outputs = Hash.new
+bots = Hash.new([])
+instrs = Hash.new([])
+outputs = Hash.new([])
 
 lines.each do |line|
   if line =~ /bot (\d+) gives low to (\w+) (\d+) and high to (\w+) (\d+)/
-    instrs[$1] ||= Set.new
-    instrs[$1].add [$2, $3, $4, $5]
+    instrs[$1] += [[$2, $3, $4, $5]]
   elsif line =~ /value (\d+) goes to bot (\d+)/
-    bots[$2] ||= Set.new
-    bots[$2].add($1.to_i)
+    bots[$2] += [$1.to_i]
   end
 end
 
@@ -33,23 +31,21 @@ while true
     min, max = chips.minmax
     chips.clear
 
-    bots[d1] ||= Set.new
-    outputs[d1] ||= Set.new
-    bots[d2] ||= Set.new
-    outputs[d2] ||= Set.new
-
     if t1 == 'bot'
-      bots[d1].add min
+      bots[d1] += [min]
+      bots[d1].sort!
     else
-      outputs[d1].add min
+      outputs[d1] += [min]
     end
     if t2 == 'bot'
-      bots[d2].add max
+      bots[d2] += [max]
+      bots[d2].sort!
     else
-      outputs[d2].add max
+      outputs[d2] += [max]
     end
   end
 end
+
 
 p outputs['0'].min * outputs['1'].min * outputs['2'].min
 
