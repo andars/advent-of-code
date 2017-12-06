3179a99ff0b8707b7e5e4b500b6ca4c3957efa49
andars <afoote97@gmail.com>
2017-12-05 22:20:09 -0700

    day06 solution

---
+++ 2017/day06/task.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+
+def hash(x):
+    return ''.join([str(c) for c in x])
+
+seen = {}
+
+with open(sys.argv[1]) as input:
+    line = input.readline()
+    membanks = [int(c) for c in line.split('\t')]
+    print(membanks)
+    count = 0
+
+    while True:
+        mi = membanks.index(max(membanks))
+        num = membanks[mi]
+        membanks[mi] = 0
+        for i in range(num):
+            membanks[(mi + i + 1) % len(membanks)] += 1
+        count += 1
+
+        h = hash(membanks)
+        seen_index = seen.get(h, None)
+
+        if seen_index != None:
+            loop_size = count - seen_index
+            break
+        else:
+            seen[hash(membanks)] = count
+
+    print("Part 1: ", count)
+    print("Part 2: ", loop_size)
+
